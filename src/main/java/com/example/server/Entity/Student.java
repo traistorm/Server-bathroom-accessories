5b7b078fce2261801f982f8a4df0922a8443f8bf
traistorm <cuongcaohuy@gmail.com>
2022-05-02 00:08:29 +0700

    make it better

---
+++ src/main/java/com/example/server/Entity/Student.java
@@ -0,0 +1,16 @@
+package com.example.server.Entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    private Long id;
+    private String name;
+}
