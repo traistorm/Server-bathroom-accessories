8023fa774ea70d2f4dd6139d000610bbabf99e1b
traistorm <cuongcaohuy@gmail.com>
2022-05-04 13:06:43 +0700

    t

--- src/main/java/com/example/server/Utilities/Function.java
+++ src/main/java/com/example/server/Utilities/Function.java
@@ -104,12 +104,12 @@ public class Function {
 
             byte[] byteJsonPayloadDecrypt = cipher.doFinal(Base64.getDecoder().decode(stringJsonPayloadEncrypt.getBytes())); // Giải mã đoạn 2 sẽ ra payload, chưa thông tin đăng nhập, vai trò, ...
             String stringJsonPayloadDecrypt = new String(byteJsonPayloadDecrypt);
-
-            byte[] byteSignatureEncrypt = cipher.doFinal(Base64.getDecoder().decode(stringSignatureEncrypt.getBytes())); // Giải mã đoạn 3 ra signature, là hợp của hai đoạn trên và thời gian tồn tại của token
-            String stringSignatureDecrypt = new String(byteSignatureEncrypt);
             return 1;
 
-            /*// Decrypt stringSignatureDecrypt we will get header + payload + token expiration time
+            /*byte[] byteSignatureEncrypt = cipher.doFinal(Base64.getDecoder().decode(stringSignatureEncrypt.getBytes())); // Giải mã đoạn 3 ra signature, là hợp của hai đoạn trên và thời gian tồn tại của token
+            String stringSignatureDecrypt = new String(byteSignatureEncrypt);
+
+            // Decrypt stringSignatureDecrypt we will get header + payload + token expiration time
             String[] stringSignatureDecryptSpilt = stringSignatureDecrypt.split("\\.");
 
             String tokenExpirationTimeEncrypt = stringSignatureDecryptSpilt[2]; // Lấy phần 3 là thời gian tồn tại của token và giải mã
