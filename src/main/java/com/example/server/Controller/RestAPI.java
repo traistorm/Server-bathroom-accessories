4f3b110f9ce84e6dad500598ac7dfcf5b159ec29
traistorm <cuongcaohuy@gmail.com>
2022-05-04 10:39:32 +0700

    test

--- src/main/java/com/example/server/Controller/RestAPI.java
+++ src/main/java/com/example/server/Controller/RestAPI.java
@@ -2,8 +2,8 @@ package com.example.server.Controller;
 
 import com.example.server.Entity.Key;
 import com.example.server.Entity.Student;
-import com.example.server.Repository.StudentRepository;
-import com.example.server.Service.KeyService;
+//import com.example.server.Repository.StudentRepository;
+//import com.example.server.Service.KeyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class RestAPI {
-    @Autowired
-    StudentRepository studentRepository;
-    @Autowired
-    KeyService keyService;
+
     @RequestMapping("/CheckKey")
     public String checkKey()
     {
@@ -57,7 +54,7 @@ public class RestAPI {
         Student student = new Student();
         student.setId(0);
         student.setName(token);
-        studentRepository.save(student);
+        //studentRepository.save(student);
         return new ResponseEntity<Student>(student, HttpStatus.OK);
     }
     @PostMapping("login")
--- src/main/java/com/example/server/Service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.server.Service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class StudentService {
-}
