f1a21c18dd3a80e1c2e07054e0ab4ef7741b936a
traistorm <cuongcaohuy@gmail.com>
2022-05-23 12:50:36 +0700

    t

--- src/main/java/com/example/server/Controller/RestAPI.java
+++ src/main/java/com/example/server/Controller/RestAPI.java
@@ -116,7 +116,6 @@ public class RestAPI {
             // price increase, price decrease and most viewed;
             if (sortType != null)
             {
-                
                 switch (sortType) {
                     case "priceIncrease":
                         return new ResponseEntity<>(bathroomAccessoriesService.findBathroomAccessoriesByNewpriceBetweenOrderByNewpriceAsc(minRange, maxRange, page, itemsperpage), HttpStatus.OK);
